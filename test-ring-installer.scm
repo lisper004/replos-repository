67e704e1efbeec16ad5f5f33121e952700d25dfe
(lisper004) <meruniaiharu@gmail.com>
2026-06-06 20:50:49 +0500

    Update test-ring-installer.scm

--- test-ring-installer.scm
+++ test-ring-installer.scm
@@ -114,13 +114,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 git clone https://github.com/lisper004/replos.git /root/.replos
 cp -r /root/.replos/.guile /root/
-              
-mkdir -p /etc/systemd/system/getty@tty1.service.d/
-cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
-[Service]
-ExecStart=
-ExecStart=-/sbin/agetty --autologin root --noclear %I \$TERM
-EOF
 
 echo 'Configuration complete.'
 ")))
